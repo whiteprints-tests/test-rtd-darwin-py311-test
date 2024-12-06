7fb2ab72158f1cffc44e89a4ed8473325bfec652
Romain Brault <mail@romainbrault.com>
2024-12-06 06:13:55 +0000

    feat: ✨ add command line interface

--- src/test_rtd_darwin_py311_test/__init__.py
+++ src/test_rtd_darwin_py311_test/__init__.py
@@ -6,8 +6,15 @@
 
 from typing import Final
 
+from test_rtd_darwin_py311_test.environment import (
+    ENVIRONMENT_FILE,
+    load_dotenv,
+)
 from test_rtd_darwin_py311_test.package_metadata import __version__
 
 
 __all__: Final = ["__version__"]
 """Public module attributes."""
+
+
+load_dotenv(ENVIRONMENT_FILE)
